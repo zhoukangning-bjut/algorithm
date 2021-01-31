36933cdbbe0c2a0659758bc440cd655718b49fbf
zhoukangning <zhoukangning_bjut@163.com>
2021-01-31 22:25:57 +0800

    quicksort

--- src/main/java/quickSort.java
+++ src/main/java/quickSort.java
@@ -1,5 +1,35 @@
 public class quickSort {
+
     public static void main(String[] args) {
-        System.out.println("1");
+        int data[] = new int[]{5, 6, 7, 4, 3};
+        sort(data,0,4);
+        for (int i = 0; i < data.length; i ++){
+            System.out.println(data[i]);
+        }
+    }
+
+    public static void sort(int[] data, int start, int end){
+        if (data == null || start > end)
+            return;
+        int array[] = data;
+        int left = start;
+        int right = end;
+        int temp = array[start];
+        while (left < right){
+            while (left < right && array[right] >= temp)
+                right --;
+            while (left < right && array[left] <= temp)
+                left ++;
+            change(data, left, right);
+        }
+        change(data, start, left);
+        sort(data, start, left - 1);
+        sort(data, left + 1, end);
+    }
+
+    public static void change(int[] data, int a, int b){
+        int temp = data[a];
+        data[a] = data[b];
+        data[b] = temp;
     }
 }
