73899ee35fbc53aaf045348db012d8132cf964c5
zhoukangning <zhoukangning_bjut@163.com>
2021-02-06 12:50:27 +0800

    选择排序

---
+++ src/main/java/SelectionSort.java
@@ -0,0 +1,27 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] data = new int[]{5, 4, 3, 2, 1};
+        for (int i = 0; i < data.length; i++){
+            selectSort(data);
+            System.out.println(data[i]);
+        }
+    }
+
+    public static void selectSort(int[] data) {
+
+        for (int i = 0; i < data.length; i++) {
+            int min = i;
+            for (int j = 0; j < data.length - i - 1; j++) {
+                if (data[j] > data[j + 1])
+                    min = j + 1;
+            }
+            change(data, i, min);
+        }
+    }
+
+    public static void change(int[] data, int a, int b) {
+        int temp = data[a];
+        data[a] = data[b];
+        data[b] = temp;
+    }
+}
